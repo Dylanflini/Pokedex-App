aafe628ea0c4301acbc0e9fecc722c393404c7a2
Dylanflini <42789063+Dylanflini@users.noreply.github.com>
2020-11-03 08:35:59 -0300

    add config for test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,42 @@
+// // module.exports = {
+// //   // [...]
+// //   globals: {
+// //     'ts-jest': {
+// //       tsConfig: 'tsconfig.test.json'
+// //     }
+// //   }
+// // };
+
+// module.exports = {
+//   // "roots": [
+//   //   "<rootDir>/src"
+//   // ],
+//   "testMatch": [
+//     "**/__tests__/**/*.+(ts|tsx|js)",
+//     "**/?(*.)+(spec|test).+(ts|tsx|js)"
+//   ],
+//   "transform": {
+//     "^.+\\.(ts|tsx)$": "ts-jest"
+//   },
+// }
+
 module.exports = {
-  // [...]
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json'
-    }
-  }
-};
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+}
